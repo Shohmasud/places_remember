252f8208ec83e164689121db1d302b618306d7c0
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 07:58:51 +0700

    Add the address name field model

--- travels_notebook/models.py
+++ travels_notebook/models.py
@@ -6,4 +6,5 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Places(models.Model):
-
+    name = models.CharField(max_length=500, blank=True,
+                            verbose_name="Places name")

de8272a7269fd83724abd6ab653223e4b4d9ff46
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 09:19:26 +0700

    Create a get() method
    This method returns a registration
    template for a get request.

--- travels_notebook/views.py
+++ travels_notebook/views.py
@@ -5,3 +5,6 @@ from django.views.generic import View, ListView, DetailView, CreateView
 # Create your views here.
 class LoginUsers(View):
     """The registration class that returns the registration template"""
+
+    def get(self, request):
+        return render(request, 'travels_notebook/html/login.html')

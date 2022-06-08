b8920a99efe353e40fe1ef2cda8ed8d797fdff85
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 08:57:36 +0700

    Add own to search for fields in the admin panel

--- travels_notebook/admin.py
+++ travels_notebook/admin.py
@@ -7,3 +7,4 @@ from travels_notebook.models import Places
 @admin.register(Places)
 class PlacesAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'description', 'time_create', 'time_update', 'relete_user')
+    search_fields = ['id', 'name']

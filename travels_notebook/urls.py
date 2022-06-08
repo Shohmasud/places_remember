613439cda90bede94334ffe9786d8f98ca8cd4d6
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 13:19:42 +0700

    Create url for PlacesList class

--- travels_notebook/urls.py
+++ travels_notebook/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from travels_notebook.views import LoginUsers
+from travels_notebook.views import LoginUsers,PlacesList
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('login/users/', LoginUsers.as_view(), name='login'),
+    path('show/list/remembers/', PlacesList.as_view(), name='home'),
 ]

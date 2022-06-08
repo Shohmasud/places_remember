c26d231337b7c3e39009c23434860016a9439ab2
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 13:44:52 +0700

    Change name path html file

--- travels_notebook/urls.py
+++ travels_notebook/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,include
-from travels_notebook.views import LoginUsers,PlacesList
+from travels_notebook.views import LoginUsers,PlacesList,ShowPostRemember
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('login/users/', LoginUsers.as_view(), name='login'),
     path('show/list/remembers/', PlacesList.as_view(), name='home'),
+    path('show/post/<slug:slug>', ShowPostRemember.as_view(), name='show-post'),
 ]

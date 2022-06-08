61d2041091affd107ee3752d6ebd591cb21aefb7
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 09:13:34 +0700

    Add registration LoginUsers(View)
    The registration class that returns the registration template.

--- travels_notebook/views.py
+++ travels_notebook/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView, CreateView
 
+
 # Create your views here.
+class LoginUsers(View):
+    """The registration class that returns the registration template"""

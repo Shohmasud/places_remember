f82cb9edf04ee373b60f171d9d5920db782bb71d
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 14:21:12 +0700

    Creating a url for getting a form-map

--- travels_notebook/urls.py
+++ travels_notebook/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path,include
-from travels_notebook.views import LoginUsers,PlacesList,ShowPostRemember
+from django.urls import path, include
+from travels_notebook.views import LoginUsers, \
+    PlacesList, ShowPostRemember, GetMap
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('login/users/', LoginUsers.as_view(), name='login'),
     path('show/list/remembers/', PlacesList.as_view(), name='home'),
+    path('page/form/map/<slug:slug>', GetMap.as_view(), name='form-page'),
     path('show/post/<slug:slug>', ShowPostRemember.as_view(), name='show-post'),
 ]

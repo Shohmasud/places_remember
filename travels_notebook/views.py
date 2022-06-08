9af2963cd2da89556bbc388c52f7b16db11948bc
Shohmasud <shohmasudz99@gmail.com>
2022-06-08 13:08:09 +0700

    Create a PlacesList class of a list of places
    This class creates a paginator and
    filters the places visited by the user

--- travels_notebook/views.py
+++ travels_notebook/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import View, ListView, DetailView, CreateView
+from travels_notebook.models import Places,User
 
 
 # Create your views here.
 class LoginUsers(View):
-    """The registration class that returns the registration template"""
+    """The registration class that returns
+    the registration template"""
 
     def get(self, request):
         return render(request, 'travels_notebook/html/login.html')
+
+
+class PlacesList(ListView):
+    """This class creates a paginator and
+    filters the places visited by the user"""
+    paginate_by = 3
+    model = Places
+    template_name = 'travels_notebook/html/home.html'
+
+    def get_queryset(self):
+        return Places.objects.filter(relete_user=User.objects.all().last())
